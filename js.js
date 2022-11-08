0c5490bf7c29c302d932cf5dc5d0896f268520a7
PlatosCodes <117208524+PlatosCodes@users.noreply.github.com>
2022-11-08 08:25:58 -0500

    Create button on top of screen and align items on center of page.

--- js.js
+++ js.js
@@ -1,12 +1,23 @@
-const container = document.querySelector(".container");
+const container = document.querySelector('.container');
 
-for (let i=0; i < 256; i++){
-const div = document.createElement("div");
-container.appendChild(div);}
+for (let i=0;i<256;i++){
+    let div = document.createElement('div');
+    div.style.cssText = "border: 1px solid rgb(53,51,51); height: 40px; width: 40px";
+    container.appendChild(div);
+}
 
-boxes = document.querySelectorAll('div');
-boxes.forEach((x) => {
-    x.addEventListener('mouseover', () => {
-        x.style.backgroundColor = 'red';
-    })
+const boxes = document.querySelectorAll('.container div');
+boxes.forEach((y) => {
+    console.log(y)
+    y.addEventListener('mouseover', () => {
+        y.style.backgroundColor = 'black';
+    });
 })
+
+newDiv = document.createElement('button');
+newDiv.textContent = "SHAKE IT UP!";
+newDiv.style.margin = '40px';
+document.body.style.display = "flex";
+document.body.style.flexDirection = 'column';
+document.body.style.alignItems = 'center';
+document.body.insertBefore(newDiv, container);
