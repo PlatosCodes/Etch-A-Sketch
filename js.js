550767793fbe1522dbb3f5bca174283b9cde2c9d
PlatosCodes <117208524+PlatosCodes@users.noreply.github.com>
2022-11-08 09:06:30 -0500

    Add button functionality to allow users to choose number of boxes per side.

--- js.js
+++ js.js
@@ -1,23 +1,50 @@
+// align items on page
+
+document.body.style.display = "flex";
+document.body.style.flexDirection = 'column';
+document.body.style.alignItems = 'center';
+
+// access container
 const container = document.querySelector('.container');
 
-for (let i=0;i<256;i++){
+//create boxes
+function createBoxes(w) {
+for (let i=0; i<w*w; i++){
     let div = document.createElement('div');
-    div.style.cssText = "border: 1px solid rgb(53,51,51); height: 40px; width: 40px";
+    div.style.cssText = `border: 1px solid rgb(53,51,51); height: ${640/w}px; width: ${640/w}px`;
     container.appendChild(div);
 }
-
+}
+//create responsive boxes
+function responsiveBoxes() {
 const boxes = document.querySelectorAll('.container div');
 boxes.forEach((y) => {
-    console.log(y)
     y.addEventListener('mouseover', () => {
         y.style.backgroundColor = 'black';
     });
 })
-
+}
+//create button
 newDiv = document.createElement('button');
 newDiv.textContent = "SHAKE IT UP!";
 newDiv.style.margin = '40px';
-document.body.style.display = "flex";
-document.body.style.flexDirection = 'column';
-document.body.style.alignItems = 'center';
+
+function shakeIt(){
+    w = prompt('How many boxes should each side have? Please enter a number less than 100.');
+    if (w >= 100) {
+        shakeIt();
+    }
+    else {
+    container.replaceChildren();
+    createBoxes(w);
+    container.style.gridTemplateColumns = `repeat(${w}, ${640/w}px [col-start])`;
+    responsiveBoxes();
+    }
+
+}
+
+newDiv.onclick = function () { 
+    shakeIt()
+    };
+
 document.body.insertBefore(newDiv, container);
